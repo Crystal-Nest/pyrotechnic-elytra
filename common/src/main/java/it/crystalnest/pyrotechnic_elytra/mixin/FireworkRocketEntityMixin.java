2d90b1f165a8d8b36d013191be038ea1df73f108
Noir <60018820+sZeta99@users.noreply.github.com>
2024-05-03 20:10:28 +0200

    Mixin
    Signed-off-by: Noir <60018820+sZeta99@users.noreply.github.com>

---
+++ common/src/main/java/it/crystalnest/pyrotechnic_elytra/mixin/FireworkRocketEntityMixin.java
@@ -0,0 +1,28 @@
+package it.crystalnest.pyrotechnic_elytra.mixin;
+
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.FireworkRocketEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * Mixin for {@link FireworkRocketEntity} to prevent firework damage when flying.
+ */
+@Mixin(FireworkRocketEntity.class)
+public class FireworkRocketEntityMixin {
+    /**
+     * <br>
+     * Removes the call to {@code hurt} and prevents the damage.
+     *
+     * @param shooter Shooter of the firework.
+     * @param damageSource Firework damage source.
+     * @param damage Damage amount.
+     * @return {@code false} because no damage will take place.
+     */
+    @Redirect(method = "dealExplosionDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", ordinal = 0))
+    private boolean preventHurt(LivingEntity shooter, DamageSource damageSource, float damage) {
+        return false;
+    }
+}
